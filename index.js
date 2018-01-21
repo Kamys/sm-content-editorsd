9473f2937ceade3f21649082f36b992b4036db42
Kamys <nikita.kamyshenko@mail.ru>
2018-01-22 00:21:20 +0300

    Add bootstrap,jquery-ui,jquery

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+window.$ = window.jQuery = require("jquery");
+require("jquery-ui-bundle/jquery-ui.min");
+require("jquery-ui-bundle/jquery-ui.css");
+require("bootstrap/dist/js/bootstrap.bundle.min");
+require("bootstrap/dist/css/bootstrap.min.css");
