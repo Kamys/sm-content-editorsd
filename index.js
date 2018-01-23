9673f81fdb3f5b41e5177033a531e7b8cb8b0602
Nikita <nikita.kamyshenko@mail.ru>
2018-01-23 19:49:01 +0300

    Add icon

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ window.$ = window.jQuery = require("jquery");
 require("jquery-ui-bundle/jquery-ui.min");
 require("jquery-ui-bundle/jquery-ui.css");
 require("bootstrap/dist/js/bootstrap.bundle.min");
+
+/* Style */
 require("bootstrap/dist/css/bootstrap.min.css");
+require("./common.blocks/tool-bar/tool-bar.css");
+require("./node_modules/font-awesome/css/font-awesome.min.css");
+require("./node_modules/font-awesome/fonts/fontawesome-webfont.svg");
--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,25 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        library: "myLibs"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/, use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(jpe?g|png|gif|resvg)$/i,
-                use: [
-                    'url-loader?limit=10000',
-                    'img-loader'
-                ]
-            }
-        ]
-    }
+	entry: './index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		library: "myLibs"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/, use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(jpe?g|png|gif|resvg)$/i,
+				use: ['url-loader?limit=10000', 'img-loader']
+			},
+			{
+				test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+				loader: 'url-loader'
+			}
+		]
+	}
 };
